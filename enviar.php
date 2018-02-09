611e7d9c4c514c543a2f289d439b2a4fa7829d2d
José Wilber Macedo <35205922+WilberMacedo@users.noreply.github.com>
2018-02-08 23:40:59 -0600

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,16 +1,15 @@
-<?
-$servidor = "localhost";
+<?php
+$ip = 'localhost';
 $usuario = 'root';
 $password = '';
 $db_name = 'db_santgar';
+$conn = mysql_pconnect($ip,$usuario,$password) or die("Problema al conectar con el host");
 
-
-$mysqli = new mysqli(servidor,usuario,password) or die();
-
-$huboerror = mysql_select_db($db_name,$mysqli) or die();
+$huboerror = mysql_select_db($db_name,$conn) or die("Problema al conectar con la base de datos");
 
 if ( !empty($_POST['submit']) ) {
-$query = "INSERT INTO `resultados` (email,nombre,apellidoPat,apellidoMat,estado) values ('{$_POST['email']}','{$_POST['nombre']}','{$_POST['apellidoPat']}','}{$_POST['apellidoMat']}','}{$_POST['estado']}')";
-$response = mysql_query($query, $mysqli);
-}
+$query = "INSERT INTO resultados (email,nombre,apellidoPat,apellidoMat,estado) values ('{$_POST['email']}','{$_POST['nombre']}','{$_POST['apellidoPat']}','{$_POST['apellidoMat']}','{$_POST['estado']}')";
+mysql_query($query, $conn);
+	}
+	echo "Los datos han sido insertados correctamente"
 ?>
